fc734add0169ea05bec7428a7a6f2957736f9e23
Hill Onyechekwa <hillonyechekwa@gmail.com>
2021-09-02 15:57:38 +0100

    styled content info card in index page

--- src/components/bottominfocont.js
+++ src/components/bottominfocont.js
@@ -2,46 +2,54 @@ import React from "react"
 import Avatar from "./svgs/avatar"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { fab } from "@fortawesome/free-brands-svg-icons"
-import { faGithubAlt, faCodepen, faTwitter, faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons"
+import {
+  faGithubAlt,
+  faCodepen,
+  faTwitter,
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons"
 
 const BottomInfoCont = () => {
   return (
-    <>
+    <div className="content-wrap">
       <Avatar />
-      <h2>Say Hello 👋</h2>
-      <p className="body1">
-        if you have conversations on certain topics to converse about or you
-        just want to see my projects click any of the links to my other
-        platforms below.
-      </p>
-      <ul>
-        <li>
-                  <a href="">
-                      <FontAwesomeIcon icon={faGithubAlt} />
-          </a>
-        </li>
-        <li>
-                  <a href="">
-                      <FontAwesomeIcon icon={faCodepen} />
-          </a>
-        </li>
-        <li>
-                  <a href="">
-                      <FontAwesomeIcon icon={faTwitter} />
-          </a>
-        </li>
-        <li>
-                  <a href="">
-                      <FontAwesomeIcon icon={faFacebook} />
-          </a>
-        </li>
-        <li>
-                  <a href="">
-                      <FontAwesomeIcon icon={faInstagram} />
-          </a>
-        </li>
-      </ul>
-    </>
+      <div>
+        <h2>Say Hello 👋🏾</h2>
+        <p className="body1">
+          if you have conversations on certain topics to converse about or you
+          just want to see my projects click any of the links to my other
+          platforms below.
+        </p>
+        <ul>
+          <li>
+            <a href="https://github.com">
+              <FontAwesomeIcon icon={faGithubAlt} size="2x"/>
+            </a>
+          </li>
+          <li>
+            <a href="https://codepen.com">
+              <FontAwesomeIcon icon={faCodepen} size="2x"/>
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com">
+              <FontAwesomeIcon icon={faTwitter} size="2x"/>
+            </a>
+          </li>
+          <li>
+            <a href="https://facebook.com">
+              <FontAwesomeIcon icon={faFacebook} size="2x"/>
+            </a>
+          </li>
+          <li>
+            <a href="https://instagram.com">
+              <FontAwesomeIcon icon={faInstagram} size="2x"/>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
