8a4ac7609667c8d4454354d92019d976f37be405
Hill Onyechekwa <hillonyechekwa@gmail.com>
2021-09-02 17:39:25 +0100

    added tooltips to the contact icons in content info card

--- src/components/bottominfocont.js
+++ src/components/bottominfocont.js
@@ -9,6 +9,7 @@ import {
   faFacebook,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons"
+import ReactTooltip from "react-tooltip"
 
 const BottomInfoCont = () => {
   return (
@@ -23,29 +24,44 @@ const BottomInfoCont = () => {
         </p>
         <ul>
           <li>
-            <a href="https://github.com">
-              <FontAwesomeIcon icon={faGithubAlt} size="2x"/>
-            </a>
+            <a data-tip data-for="github-icon" href="https://github.com">
+              <FontAwesomeIcon icon={faGithubAlt} size="2x" />
+                      </a>
+                      <ReactTooltip place="top" id="github-icon" effect="float" type="dark">
+                          Github
+                      </ReactTooltip>
           </li>
           <li>
-            <a href="https://codepen.com">
-              <FontAwesomeIcon icon={faCodepen} size="2x"/>
-            </a>
+            <a data-tip data-for="codepen-icon" href="https://codepen.com">
+              <FontAwesomeIcon icon={faCodepen} size="2x" />
+                      </a>
+                      <ReactTooltip place="top" id="codepen-icon" effect="float" type="dark">
+                          Codepen
+                      </ReactTooltip>
           </li>
           <li>
-            <a href="https://twitter.com">
-              <FontAwesomeIcon icon={faTwitter} size="2x"/>
-            </a>
+            <a data-tip data-for="twitter-icon" href="https://twitter.com">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+                      </a>
+                      <ReactTooltip place="top" id="twitter-icon" effect="solid" type="dark">
+                          Twitter
+                      </ReactTooltip>
           </li>
           <li>
-            <a href="https://facebook.com">
-              <FontAwesomeIcon icon={faFacebook} size="2x"/>
+            <a data-tip data-for="facebook-icon" href="https://facebook.com">
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
             </a>
+            <ReactTooltip place="top" id="facebook-icon" effect="solid" type="dark">
+                          Facebook
+                      </ReactTooltip>
           </li>
           <li>
-            <a href="https://instagram.com">
-              <FontAwesomeIcon icon={faInstagram} size="2x"/>
-            </a>
+            <a data-tip data-for="instagram-icon"  href="https://instagram.com">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+                      </a>
+                      <ReactTooltip place="top" id="instagram-icon" effect="solid" type="dark">
+                          Instagram
+                      </ReactTooltip>
           </li>
         </ul>
       </div>
